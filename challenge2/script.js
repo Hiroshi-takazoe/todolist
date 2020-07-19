e080668c1199801613db3b6d5210258e26bb85c5
高添　浩司 <takazoe@MacBook-Pro.local>
2020-07-19 16:23:12 +0900

    modified2

--- challenge2/script.js
+++ challenge2/script.js
@@ -1,36 +1,44 @@
 const table = document.getElementById("task");
 const addButton = document.getElementById("addButton");
+const todos = [];
+let todoslength = 0;
 
-
-addButton.addEventListener("click", addRow);
-
-function addRow() {
-  const rowNumber = table.rows.length;
-  let id = rowNumber - 1;
+addButton.addEventListener("click", function() {
   let comment = document.getElementById("comment").value;
-
-  let rows = table.insertRow(-1);
-  let cell1 = rows.insertCell(-1);
-  let idNumber = document.createTextNode(id);
-  cell1.appendChild(idNumber);
-
-  let cell2 = rows.insertCell(-1);
-  let addComment = document.createTextNode(comment);
-  cell2.appendChild(addComment);
-
-  let cell3 = rows.insertCell(-1);
-  let statusButton = document.createElement("input");
-  statusButton.type = "button";
-  statusButton.name = "statusButton";
-  statusButton.value = "作業中";
-  cell3.appendChild(statusButton);
-
-  let cell4 = rows.insertCell(-1);
-  let delButton = document.createElement("input");
-  delButton.type = "button";
-  delButton.name = "delButton";
-  delButton.value = "削除";
-  cell4.appendChild(delButton);
-
+  let todo = {
+    task: comment,
+    status: "作業中"
+  };
+  todos.push(todo);
   document.addForm.reset();
+  todoslength = todos.length;
+  displayTodos();
+});
+
+function displayTodos() {
+  const status = document.getElementsByName("status");
+  if (status[0].checked) {
+    let rows = table.insertRow(-1);
+    let cell1 = rows.insertCell(-1);
+    let idNumber = document.createTextNode(todoslength - 1);
+    cell1.appendChild(idNumber);
+
+    let cell2 = rows.insertCell(-1);
+    let task = document.createTextNode(todos[todoslength - 1].task);
+    cell2.appendChild(task);
+
+    let cell3 = rows.insertCell(-1);
+    let statusButton = document.createElement("input");
+    statusButton.type = "button";
+    statusButton.name = "statusButton";
+    statusButton.value = todos[todoslength - 1].status;
+    cell3.appendChild(statusButton);
+
+    let cell4 = rows.insertCell(-1);
+    let delButton = document.createElement("input");
+    delButton.type = "button";
+    delButton.name = "delButton";
+    delButton.value = "削除";
+    cell4.appendChild(delButton);
+  };
 }
