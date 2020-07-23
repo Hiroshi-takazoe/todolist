ccb54f1607d39871664db501912ae9e9b91cffc2
高添　浩司 <takazoe@MacBook-Pro.local>
2020-07-23 18:52:10 +0900

    challenge2-2

--- challenge2/script.js
+++ challenge2/script.js
@@ -1,7 +1,6 @@
 const table = document.getElementById("task");
 const addButton = document.getElementById("addButton");
 const todos = [];
-let todoslength = 0;
 
 addButton.addEventListener("click", function() {
   let comment = document.getElementById("comment").value;
@@ -11,27 +10,29 @@ addButton.addEventListener("click", function() {
   };
   todos.push(todo);
   document.addForm.reset();
-  todoslength = todos.length;
   displayTodos();
 });
+
+
 
 function displayTodos() {
   const status = document.getElementsByName("status");
   if (status[0].checked) {
     let rows = table.insertRow(-1);
     let cell1 = rows.insertCell(-1);
-    let idNumber = document.createTextNode(todoslength - 1);
+    cell1.className = "sequence";
+    let idNumber = document.createTextNode(todos.length - 1);
     cell1.appendChild(idNumber);
 
     let cell2 = rows.insertCell(-1);
-    let task = document.createTextNode(todos[todoslength - 1].task);
+    let task = document.createTextNode(todos[todos.length - 1].task);
     cell2.appendChild(task);
 
     let cell3 = rows.insertCell(-1);
     let statusButton = document.createElement("input");
     statusButton.type = "button";
     statusButton.name = "statusButton";
-    statusButton.value = todos[todoslength - 1].status;
+    statusButton.value = todos[todos.length - 1].status;
     cell3.appendChild(statusButton);
 
     let cell4 = rows.insertCell(-1);
@@ -40,5 +41,26 @@ function displayTodos() {
     delButton.name = "delButton";
     delButton.value = "削除";
     cell4.appendChild(delButton);
+    delButton.onclick = deleteTask;
   };
+}
+
+function deleteTask(event) {
+  let tr = event.target.parentNode.parentNode;
+  let text = this.parentNode.previousElementSibling.previousElementSibling.textContent;
+  let index = todos.findIndex(({task}) => task === text);
+  tr.remove();
+  todos.splice(index, 1);
+  reNumber();
+
+}
+
+function reNumber(){
+  let tdNumber =document.getElementsByClassName("sequence");
+  let idNumber = 0;
+  for (var i = 0; i < todos.length; i++){
+    let newIdNumber = document.createTextNode(idNumber);
+    tdNumber[i].textContent = idNumber;
+    idNumber++;
+  }
 }
