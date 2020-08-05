1f91f87405372c4b344d31417c6728f3e02c1ef5
高添　浩司 <takazoe@MacBook-Pro.local>
2020-08-05 21:43:12 +0900

    challenge2-4

--- challenge2/script.js
+++ challenge2/script.js
@@ -3,7 +3,40 @@ const addButton = document.getElementById('addButton');
 const regex = /^\s+$/;
 const working = '作業中';
 const complete = '完了';
+const status = document.getElementsByName('status');
 const todos = [];
+
+status[0].addEventListener('click', () => {
+  for (i = 1; i <= todos.length; i++) {
+    table.rows[i].style.display = "";
+  }
+});
+
+status[1].addEventListener('click', () => {
+
+  for (let i = 1; i <= todos.length; i++) {
+    let tableRow = table.rows[i];
+    let isStatus = table.children[0].children[i].children[2].children[0].value;
+    if (isStatus === "完了") {
+      tableRow.style.display = "none";
+    } else if (isStatus === "作業中") {
+      tableRow.style.display = "";
+    }
+  }
+});
+
+status[2].addEventListener('click', () => {
+
+  for (let i = 1; i <= todos.length; i++) {
+    let tableRow = table.rows[i];
+    let isStatus = table.children[0].children[i].children[2].children[0].value;
+    if (isStatus === "作業中") {
+      tableRow.style.display = "none";
+    } else if (isStatus === "完了") {
+      tableRow.style.display = "";
+    }
+  }
+});
 
 addButton.addEventListener('click', () => {
   const comment = document.getElementById('comment').value;
@@ -17,31 +50,36 @@ addButton.addEventListener('click', () => {
     };
     todos.push(todo);
     document.addForm.reset();
-    displayTodos();
+    if (status[0].checked) {
+      displayTodos();
+    } else if (status[1].checked) {
+      displayTodos();
+    } else {
+      displayTodos();
+      table.rows[todos.length].style.display = "none";
+    }
   }
 });
 
 const displayTodos = () => {
-  const status = document.getElementsByName('status');
-  if (status[0].checked) {
-    const rows = table.insertRow(-1);
-    const cell1 = rows.insertCell(-1);
-    cell1.className = 'sequence';
-    const idNumber = document.createTextNode(String(todos.length - 1));
-    cell1.appendChild(idNumber);
 
-    const cell2 = rows.insertCell(-1);
-    const task = document.createTextNode(String(todos[todos.length - 1].task));
-    cell2.appendChild(task);
+  const rows = table.insertRow(-1);
+  const cell1 = rows.insertCell(-1);
+  cell1.className = 'sequence';
+  const idNumber = document.createTextNode(String(todos.length - 1));
+  cell1.appendChild(idNumber);
 
-    const cell3 = rows.insertCell(-1);
-    const stateBtn = createStateBtn();
-    cell3.appendChild(stateBtn);
+  const cell2 = rows.insertCell(-1);
+  const task = document.createTextNode(String(todos[todos.length - 1].task));
+  cell2.appendChild(task);
 
-    const cell4 = rows.insertCell(-1);
-    const deleteBtn = createDeleteBtn();
-    cell4.appendChild(deleteBtn);
-  }
+  const cell3 = rows.insertCell(-1);
+  const stateBtn = createStateBtn();
+  cell3.appendChild(stateBtn);
+
+  const cell4 = rows.insertCell(-1);
+  const deleteBtn = createDeleteBtn();
+  cell4.appendChild(deleteBtn);
 }
 
 const createStateBtn = () => {
